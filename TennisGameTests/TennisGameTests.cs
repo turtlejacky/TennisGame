2bfab2f5f69e4f664324a4175aabce3035ee4736
neil.tao <neil.tao@titansoft>
2018-11-20 22:24:40 +0800

    add basic test file

---
+++ TennisGameTests/TennisGameTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameTests
+{
+    [TestClass()]
+    public class TennisGameTests
+    {
+        [TestMethod()]
+        public void Fifteen_Love()
+        {
+           
+        }
+
+    }
+}
